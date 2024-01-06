885cd0364402db1a2277640f8b0c6a407317be6f
Celal <elvercelal@gmail.com>
2024-01-06 20:35:52 +0300

    gezinomi_project
    Python Data_Science_Project

---
+++ gezinomi_data_science/gezinomi_data_science.py
@@ -0,0 +1,120 @@
+#######################
+# Potential Customer Yield Calculation with Rule-Based Classification
+#######################
+
+#######################
+# Business Problem
+#######################
+# Gezinomi creates new level-based sales definitions by using some features of the sales it makes.
+# To create and create segments according to these new sales definitions and to attract new customers to the company according to these segments.
+# wants to estimate how much money it can earn on average.
+# For example: It is desired to determine how much a customer who wants to go to an All Inclusive hotel from Antalya during a busy period can earn on average.
+#############################################
+# PROJECT TASKS
+#######################
+
+#######################
+# TASK 1: Answer the following questions.
+#######################
+# Question 1: Read the miuul_gezinomi.xlsx file and show general information about the data set.
+import pandas as pd
+pd.set_option("display.max_rows", None)
+pd.set_option("display.max_columns", None)
+df = pd.read_excel('dataset/miuul_gezinomi.xlsx')
+pd.set_option('display.float_format', lambda x: '%.2f' % x)
+print(df.head())
+print(df.shape)
+print(df.info())
+
+
+# Question 2: How many unique cities are there? What are their frequencies?
+print(df["SaleCityName"].nunique())
+print(df["SaleCityName"].value_counts())
+
+# Question 3: How many unique Concepts are there?
+df["ConceptName"].nunique()
+
+# Question 4: How many sales were made from which Concept?
+df["ConceptName"].value_counts()
+
+# Question 5: How much was earned from sales in total by city?
+df.groupby("SaleCityName").agg({"Price": "sum"})
+
+# Question 6: How much was earned according to concept types?
+df.groupby("ConceptName").agg({"Price": "sum"})
+
+# Question 7: What are the PRICE averages by city?
+df.groupby(by=['SaleCityName']).agg({"Price": "mean"})
+
+# Question 8: What are the PRICE averages according to concepts?
+df.groupby(by=['ConceptName']).agg({"Price": "mean"})
+
+# Question 9: What are the PRICE averages in the City-Concept breakdown?
+df.groupby(by=["SaleCityName", 'ConceptName']).agg({"Price": "mean"})
+
+
+#############################################
+# TASK 2: Convert the sales_checkin_day_diff variable into a new categorical variable called EB_Score.
+#############################################
+bins = [-1, 7, 30, 90, df["SaleCheckInDayDiff"].max()]
+labels = ["Last Minuters", "Potential Planners", "Planners", "Early Bookers"]
+
+df["EB_Score"] = pd.cut(df["SaleCheckInDayDiff"], bins, labels=labels)
+df.head(50).to_excel("eb_scorew.xlsx", index=False)
+#############################################
+# TASK 3: Look at wage averages and frequencies in City, Concept, [EB_Score, Season, CInday] breakdown
+#############################################
+# Wage averages in City-Concept-EB Score breakdown
+df.groupby(by=["SaleCityName", 'ConceptName', "EB_Score" ]).agg({"Price": ["mean", "count"]})
+
+# Wage averages by City-Concept-Season
+df.groupby(by=["SaleCityName", "ConceptName", "Seasons"]).agg({"Price": ["mean", "count"]})
+
+# Wage averages in City-Concept-Cinday breakdown
+df.groupby(by=["SaleCityName", "ConceptName", "CInDay"]).agg({"Price": ["mean", "count"]})
+
+
+#######################
+# TASK 4: Sort the output of the City-Concept-Season breakdown according to PRICE.
+#######################
+# To better see the output in the previous question, apply the sort_values ​​method to PRICE in decreasing order.
+# Save the output as agg_df.
+
+agg_df = df.groupby(["SaleCityName", "ConceptName", "Seasons"]).agg({"Price": "mean"}).sort_values("Price", ascending=False)
+agg_df.head(20)
+
+#############################################
+# TASK 5: Convert the names in the index into variable names.
+#######################
+# All variables except PRICE in the output of the third question are index names.
+# Convert these names to variable names.
+agg_df.reset_index(inplace=True)
+
+agg_df.head()
+#############################################
+# TASK 6: Define new level based sales and add them to the data set as a variable.
+#############################################
+# Define a variable called sales_level_based and add this variable to the data set.
+agg_df['sales_level_based'] = agg_df[["SaleCityName", "ConceptName", "Seasons"]].agg(lambda x: '_'.join(x).upper(), axis=1)
+
+
+#############################################
+# TASK 7: Divide the personas into segments.
+#############################################
+# Segment by PRICE,
+# add the segments to agg_df with the name "SEGMENT"
+# describe the segments
+agg_df["SEGMENT"] = pd.qcut(agg_df["Price"], 4, labels=["D", "C", "B", "A"])
+agg_df.head(30)
+agg_df.groupby("SEGMENT").agg({"Price": ["mean", "max", "sum"]})
+
+#############################################
+# TASK 8: Sort the final df according to the price variable.
+# In which segment is "ANTALYA_HERŞEY DAHIL_HIGH" and how much does it cost?
+#############################################
+agg_df.sort_values(by="Price")
+
+
+new_user = "ANTALYA_HERŞEY DAHIL_HIGH"
+agg_df[agg_df["sales_level_based"] == new_user]
+
---
+++ gezinomi_data_science/gezinomi_readme.md
@@ -0,0 +1,94 @@
+
+# Project Title
+                               With Rule-Based Classification 
+                            Calculating Potential Customer Return
+
+                            
+
+
+## Business Problem
+
+Gezinomi uses some features of its sales to make level-based sales.
+(level based) to create new sales definitions and
+create segments according to their definitions and create new segments based on these segments.
+How much money can prospective customers bring to the company on average?
+He wants to guess.
+
+FOR EXAMPLE: 
+
+Those who want to go to an All Inclusive hotel from Antalya during a busy period
+Determining how much money a customer can earn on average is wanted.
+
+Gezinomi_miuul.xlsx data set shows the prices of sales made by Gezinomi company and this
+Contains information about sales. The data set consists of records created in each sales transaction.
+is occurring. This means the table is not deduplicated. In other words
+The customer may have made more than one purchase
+
+## Variables
+#### SaleId: Sale id
+
+#### SaleDate: Sale Date
+
+#### Price: The price paid for the sale
+
+#### ConceptName:Hotel concept information
+
+#### SaleCityName: Information about the city where the hotel is located
+
+#### CheckInDate: The date the customer checked into the hotel
+
+#### CInDay: The day the customer checks in to the hotel
+
+#### SaleCheckInDayDiff: Day difference between check-in and check-in date
+
+#### Season: Season information on the hotel check-in date
+## Variables
+#### SaleId: Sale id
+
+#### SaleDate: Sale Date
+
+#### Price: The price paid for the sale
+
+#### ConceptName:Hotel concept information
+
+#### SaleCityName: Information about the city where the hotel is located
+
+#### CheckInDate: The date the customer checked into the hotel
+
+#### CInDay: The day the customer checks in to the hotel
+
+#### SaleCheckInDayDiff: Day difference between check-in and check-in date
+
+#### Season: Season information on the hotel check-in date
+## Project Tasks
+Question 1: Read the miuul_gezinomi.xlsx file and show general information about the data set.
+
+Question 2: How many unique cities are there? What are their frequencies?
+
+Question 3: How many unique Concepts are there?
+
+Question4: How many sales were made from which Concept?
+
+Question5: How much was earned from sales in total by city?
+
+Question6: How much was earned according to concept types?
+
+Question7: What are the PRICE averages by city?
+
+Question 8: What are the PRICE averages according to concepts?
+
+Question 9: What are the PRICE averages in the City-Concept breakdown?
+## Project Tasks 2
+Convert the SaleCheckInDayDiff variable to a categorical variable
+## Project Tasks 3
+What are the average earnings in the COUNTRY, SOURCE, SEX, AGE breakdown?
+## Project Tasks 4
+Sort the output of the City-Concept-Season breakdown according to PRICE.
+## Project Tasks 5
+Convert the names in the index to variable names.
+## Project Tasks 6
+Define new level-based customers (personas).
+## Project Tasks 7
+Divide new customers (personas) into segments.
+## Project Tasks 8
+Classify new customers and estimate how much revenue they can bring.
